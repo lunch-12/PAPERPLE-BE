6af51626d0bc418a6411d91a181c8ff6ab24d095
cjm0611 <cjm-0611@naver.com>
2024-08-05 17:06:07 +0900

    chore: 스프링 부트 프로젝트 구성

---
+++ src/main/java/com/ktb/paperplebe/PaperpleBeApplication.java
@@ -0,0 +1,13 @@
+package com.ktb.paperplebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperpleBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperpleBeApplication.class, args);
+    }
+
+}
