34caf7a9a8de0c46e76a187f51c06eeef24ec19c
SSUHYUNKIM <yellosuhyun@naver.com>
2024-08-12 17:14:26 +0900

    [FEAT] PaperController 추가 (#9)

---
+++ src/main/java/com/ktb/paperplebe/paper/controller/PaperController.java
@@ -0,0 +1,38 @@
+package com.ktb.paperplebe.paper.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/paper")
+@RestController
+public class PaperController {
+    private final PaperService paperService;
+
+    @PostMapping
+    public ResponseEntity<?> createPaper(@RequestBody final PaperRequest paperRequest) {
+        final PaperResponse paperResponse = paperService.createPaper(paperRequest);
+        return ResponseEntity.ok(paperResponse);
+    }
+
+    @GetMapping("/{paperId}")
+    public ResponseEntity<?> getPaper(@PathVariable Long paperId) {
+        final PaperResponse paperResponse = paperService.getPaper(paperId);
+        return ResponseEntity.ok(paperResponse);
+    }
+
+    @PatchMapping("/{paperId}/edit")
+    public ResponseEntity<?> updateRoom(
+            @PathVariable Long paperId,
+            @RequestBody final PaperRequest paperRequest) {
+        final PaperResponse paperResponse = paperService.updatePaper(paperId, paperRequest);
+        return ResponseEntity.ok(paperResponse);
+    }
+
+    @DeleteMapping("/{paperId}")
+    public ResponseEntity<String> deletePaper(@PathVariable Long paperId) {
+        paperService.deletePaper(paperId);
+        return ResponseEntity.ok("Paper successfully deleted");
+    }
+}
