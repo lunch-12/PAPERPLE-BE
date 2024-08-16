b2f931a7a6ab6794612ce3d33e966b7cfc9b7dd7
cjm0611 <cjm-0611@naver.com>
2024-08-16 17:05:05 +0900

    feat: User entity 추가

---
+++ src/main/java/com/ktb/paperplebe/user/constant/UserRole.java
@@ -0,0 +1,10 @@
+package com.ktb.paperplebe.user.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserRole {
+    ROLE_USER, ROLE_ADMIN
+}
