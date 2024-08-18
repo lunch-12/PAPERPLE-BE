f147059461821c7ef0d5f1a2c53fe5faf6734374
SSUHYUNKIM <yellosuhyun@naver.com>
2024-08-19 04:16:48 +0900

    [FEAT] SuccessResponseDTO 추가 (#10)

---
+++ src/main/java/com/ktb/paperplebe/common/dto/SuccessResponseDTO.java
@@ -0,0 +1,20 @@
+package com.ktb.paperplebe.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class SuccessResponseDTO {
+    private boolean isSuccessful = true;
+    private Integer statusCode = 200;
+
+    public SuccessResponseDTO(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public SuccessResponseDTO() {
+    }
+}
