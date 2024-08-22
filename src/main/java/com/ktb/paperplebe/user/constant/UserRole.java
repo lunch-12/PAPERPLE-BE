ed4a5eb91b992c1a5b1785f6de1a05debc8405f6
cjm0611 <cjm-0611@naver.com>
2024-08-22 15:44:14 +0900

    feat: 사용자 정보, 닉네임 업데이트 기능 구현

--- src/main/java/com/ktb/paperplebe/user/constant/UserRole.java
+++ src/main/java/com/ktb/paperplebe/user/constant/UserRole.java
@@ -6,5 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum UserRole {
-    ROLE_USER, ROLE_ADMIN
+    ROLE_USER, ROLE_ADMIN;
+
+    public static final String ROLE_USER_VALUE = "ROLE_USER";
+    public static final String ROLE_ADMIN_VALUE = "ROLE_ADMIN";
 }
