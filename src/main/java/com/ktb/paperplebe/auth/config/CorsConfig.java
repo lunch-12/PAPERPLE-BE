f8dc503d39eb3257eb7c14bc3fd05b71d175f5f4
cjm0611 <cjm-0611@naver.com>
2024-08-28 09:14:40 +0900

    feat: CORS 허용 도메인 설정과 허용 및 노출 헤더 변경
    - @Value를 사용하여 설정 파일에서 허용된 도메인을 가져오도록 변경
    - 허용 및 노출 헤더를 업데이트

--- src/main/java/com/ktb/paperplebe/auth/config/CorsConfig.java
+++ src/main/java/com/ktb/paperplebe/auth/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.ktb.paperplebe.auth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,16 +12,19 @@ import java.util.List;
 
 @Configuration
 public class CorsConfig {
+    @Value("${cors.allowed-origins.list}")
+    private List<String> allowedOrigins;
+
     @Bean(name = "corsConfigurationSource")
     @Profile({"local", "dev"})
     public CorsConfigurationSource localDevCorsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(List.of("http://127.0.0.1:3000", "http://localhost:3000", "http://localhost:8080", "https://localhost:3000"));
+        configuration.setAllowedOrigins(allowedOrigins);
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setExposedHeaders(List.of("*"));
+        configuration.setAllowedHeaders(List.of("Content-Type", "Authorization", "Accept", "Origin"));
+        configuration.setExposedHeaders(List.of("Authorization", "Location", "X-Custom-Header"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
@@ -36,8 +40,8 @@ public class CorsConfig {
         configuration.setAllowCredentials(true);
         // TO DO - 배포한 prod 도메인 주소 추가
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setExposedHeaders(List.of("*"));
+        configuration.setAllowedHeaders(List.of("Content-Type", "Authorization", "Accept", "Origin"));
+        configuration.setExposedHeaders(List.of("Authorization", "Location", "X-Custom-Header"));
         configuration.setMaxAge(1800L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
