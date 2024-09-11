7d093112e384aab8e56c7a8798875fbede5e510e
cjm0611 <cjm-0611@naver.com>
2024-09-11 14:29:01 +0900

    feat: jwt 필터에 Slf4j 로그 추가

--- src/main/java/com/ktb/paperplebe/auth/config/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/ktb/paperplebe/auth/config/jwt/JwtAuthorizationFilter.java
@@ -12,6 +12,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -26,6 +27,7 @@ import static com.ktb.paperplebe.auth.config.jwt.JwtUtil.ACCESS_TOKEN;
 import static com.ktb.paperplebe.auth.config.jwt.JwtUtil.REFRESH_TOKEN;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
@@ -37,23 +39,29 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws IOException, ServletException {
         // h2-console 요청은 토큰 검사를 수행하지 않음
+        log.info("doFilterInternal - 토큰 검사 수행");
         String requestURI = request.getRequestURI();
         if (requestURI.startsWith("/h2-console") || requestURI.startsWith("/health") ) {
+            log.info("토큰 검사를 수행하지 않고 넘어감.");
             filterChain.doFilter(request, response);
             return;
         }
 
         Cookie[] cookies = request.getCookies();
         if (cookies == null) {
+            log.info("request의 쿠키가 비어있음.");
             filterChain.doFilter(request, response);
             return;
         }
 
         String accessToken = extractCookie(cookies, ACCESS_TOKEN);
         String refreshToken = extractCookie(cookies, REFRESH_TOKEN);
+        log.info("accessToken: " + accessToken);
+        log.info("refreshToken: " + refreshToken);
 
         // case1. accessToken이 유효한 경우
         if (accessToken != null && jwtUtil.validateToken(accessToken)) {
+            log.info("case1");
             setAuthInSecurityContext(accessToken);
             filterChain.doFilter(request, response);
             return;
@@ -61,6 +69,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
         // case2. accessToken이 유효하지 않고 refreshToken이 유효하면 accessToken 갱신
         if (refreshToken != null && jwtUtil.validateToken(refreshToken)) {
+            log.info("case2");
             accessToken = tokenService.renewToken(response, refreshToken);
             setAuthInSecurityContext(accessToken);
             filterChain.doFilter(request, response);
@@ -69,10 +78,12 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
         // case3. accessToken과 refreshToken이 모두 유효하지 않으면 에러 응답 반환
         if (accessToken != null) {
+            log.info("case3");
             cookieService.deleteCookie(response, ACCESS_TOKEN);
         }
 
         if (!jwtUtil.validateToken(refreshToken)) {
+            log.info("refresh token이 유효하지 않음.");
             cookieService.deleteCookie(response, REFRESH_TOKEN);
             tokenService.deleteRefreshToken(refreshToken);
         }
