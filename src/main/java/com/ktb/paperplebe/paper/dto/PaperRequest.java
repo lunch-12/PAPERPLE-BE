bbee7344d27e1e4e2e98d6fc7e1d4d1b7da2beeb
SSUHYUNKIM <yellosuhyun@naver.com>
2024-09-11 14:23:34 +0900

    [REFACTOR] PaperRequest 수정

--- src/main/java/com/ktb/paperplebe/paper/dto/PaperRequest.java
+++ src/main/java/com/ktb/paperplebe/paper/dto/PaperRequest.java
@@ -1,9 +1,11 @@
 package com.ktb.paperplebe.paper.dto;
 
+import java.util.List;
+
 public record PaperRequest(
         String content,
         String newspaperLink,
-        int view,
+        List<String> tags,
         String newspaperSummary,
         String image
 ) {
